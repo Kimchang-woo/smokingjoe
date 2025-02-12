eac0433e48d6f63984411bf313bc2b0e7432e1f7
kimjh1107 <wjdgjs0599@gmail.com>
2025-02-12 18:38:55 +0900

    finish

--- js/tour.js
+++ js/tour.js
@@ -26,6 +26,27 @@ function hoverEffect() {
     });
 }
 
+document.addEventListener("DOMContentLoaded", function() {
+
+    function resetAnimation() {
+        const images = document.querySelectorAll('.subImage img');
+        images.forEach(image => {
+            image.style.animation = 'none'; // 애니메이션 제거
+            image.offsetHeight; // 리플로우 발생시켜 애니메이션 재시작
+            image.style.animation = ''; // 애니메이션 다시 적용
+        });
+    }
+});
+const hamberger = document.querySelector('.hamburger');
+const  modal = document.querySelector('.modal');
+const mocloseBtn =document.querySelector('#mocloseBtn');
+
+hamberger.addEventListener('click', function(){
+    modal.classList.toggle('on');
+});
+mocloseBtn.addEventListener('click', function(){
+    modal.classList.toggle('on');
+});
 hoverEffect();
 
 imgModal();
